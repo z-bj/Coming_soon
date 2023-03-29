d27ad80f5989f45c8ae48d743b808f62a299b41d
Zakaria Beji <zbjarchi@gmail.com>
2023-03-29 06:11:37 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-let countDownDate = new Date("Mar 15, 2023 09:37:25").getTime();
+let countDownDate = new Date("Apr 20, 2023 09:37:25").getTime();
 
 // Update the count down every 1 second
 let x = setInterval(function () {
